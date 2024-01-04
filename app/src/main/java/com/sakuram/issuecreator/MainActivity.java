796f12f6ef4da67f8bdf4e362d04cd145f4646be
sakuram-dev <trge241@gmail.com>
2024-01-04 17:10:17 +0900

    [issue #8]main activity

---
+++ app/src/main/java/com/sakuram/issuecreator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sakuram.issuecreator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
